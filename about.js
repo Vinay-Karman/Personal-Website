9a688b2accfe822d593c8651c5110db43cd73e63
kineticvinay <vinay2023@iisc.ac.in>
2024-06-27 07:12:14 +0530

    Optimised minimizing window interaction

--- about.js
+++ about.js
@@ -1,7 +1,22 @@
+// let dis = "0";
+
+
+// window.addEventListener("resize", function() {
+    
+//     if (window.innerWidth < 700) {
+//       dis = "100";
+//     } else {
+//       dis = "100";
+//     }
+//   });
+
+
+
+
 ScrollReveal({
     reset:true,
     duration:900,
-    distance:'100px'
+    distance: "100px"
 
 });
 
@@ -10,5 +25,5 @@ ScrollReveal().reveal('.header' );
 
 ScrollReveal().reveal('.iam , .journey , .hobby , .aspire , .connect' ,{ delay: 200 , duration: 700, origin:'left'} );
 
-ScrollReveal().reveal('.fotter-row1',{ delay:100, duration: 700,  distance:'50px' , origin:'bottom', interval:100} );
+ScrollReveal().reveal('.fotter-row1',{ delay:100, duration: 700 , distance:"50px" , origin:'bottom', interval:100} );
 
